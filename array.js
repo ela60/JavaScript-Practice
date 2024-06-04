86984510050175c37645c8d135e977d9864a3b76
Ambia-Ela <ambiaela7275@gmail.com>
2024-06-04 20:59:12 +0600

    array

--- array.js
+++ array.js
@@ -4,6 +4,7 @@ var studentName = ["one", "two", "three", "four","five"];
 
 for (i = 0; i <= studentName.length; i++){
 
-    console.log(i++  +"." +studentName[i])
+    // console.log(i++  +"." +studentName[i]) age 0 then 1 increment hoe then console i++ increment hoe 2 debe
+     console.log(i+1  +"." +studentName[i])
 }
 
